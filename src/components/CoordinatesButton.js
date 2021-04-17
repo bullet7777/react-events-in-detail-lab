d3154621aaa15f3fbf572ab5dc77bdd37534e00d
chris <chris.mahfouz@hotmail.com>
2021-04-17 14:29:12 +0300

    commit tree 96b47f2373c55e3f6264068e7bf4c417e6812c18

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,34 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+export default class CoordinatesButton extends Component {
+    constructor(){
+        super()
+        this.state={
+            mouseCoardinates:props.value
+        }
+
+    }
+    handleOnClick=(event) => {
+        this.setState({
+             arrayElement=[
+                event.clientX,
+                event.clientY
+            ]
+         
+        })
+        
+
+    }
+
+    
+  
+    
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleOnClick}></button>
+            </div>
+        )
+    }
+}
